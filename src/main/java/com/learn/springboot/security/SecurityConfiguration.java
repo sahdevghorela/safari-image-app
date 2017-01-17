1caa2e76b6ea7d65b05ddb3a4cd7ca25e2d31ef4
sahdev <sahdevghorela37@gmail.com>
2017-01-17 22:44:10 +0100

    Added spring security and thymeleaf spring security support. Added fairly simple site wise security policy to authenticate every http request.

---
+++ src/main/java/com/learn/springboot/security/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.learn.springboot.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+    /*
+    -> We Define our security policy here
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .anyRequest().authenticated()
+                    .and()
+                .formLogin()
+                    .permitAll();
+    }
+}
