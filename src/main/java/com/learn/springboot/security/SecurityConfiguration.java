d83df449bd247496a32d648b7cd4b7ea4933bfd8
sahdev <sahdevghorela37@gmail.com>
2017-01-17 22:59:44 +0100

    Added in-memory-set of users with username,password,roles and with locked and disabled accounts

--- src/main/java/com/learn/springboot/security/SecurityConfiguration.java
+++ src/main/java/com/learn/springboot/security/SecurityConfiguration.java
@@ -1,6 +1,8 @@
 package com.learn.springboot.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -17,5 +19,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
                     .and()
                 .formLogin()
                     .permitAll();
+    }
+
+
+    @Autowired
+    public void configureInMemoryUsers(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user1").password("pass").roles("ADMIN","USER")
+                .and()
+                .withUser("user2").password("pass").roles("USER")
+                .and()
+                .withUser("baduser1").password("pass").roles("USER").accountLocked(true)
+                .and()
+                .withUser("baduser2").password("pass").roles("USER").disabled(true);
+
     }
 }
